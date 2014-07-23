f29e42f55fac144177069587b57a3147b58197a0
shihang <study1228.sh@gmail.com>
2014-07-23 22:51:53 +0800

    添加了最基本的连接数据库的代码

--- src/main/java/com/baldurtech/ContactServlet.java
+++ src/main/java/com/baldurtech/ContactServlet.java
@@ -5,6 +5,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class ContactServlet extends HttpServlet
 {
@@ -12,5 +18,28 @@ public class ContactServlet extends HttpServlet
 	                  throws IOException,ServletException
 		{
 			resp.getWriter().println("get contact by id:" + req.getParameter("contactId"));
+			
+			try
+			{
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+			}
+			catch(Exception ex)
+			{
+				
+			}
+			
+			try
+			{
+				Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=");
+				resp.getWriter().println("connect sussess!!!");
+			}
+			catch(SQLException sqle)
+			{
+				resp.getWriter().println("Cannot connect to DB");
+				resp.getWriter().println(sqle.getMessage());
+				sqle.printStackTrace();
+			}
+			
 		}
+		
 }
