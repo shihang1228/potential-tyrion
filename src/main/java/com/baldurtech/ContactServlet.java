50cafcff5602dc3ce208c7cbaaf814bb0369e9ef
shihang <study1228.sh@gmail.com>
2014-07-25 13:34:38 +0800

    抽取方法：从sql查询结果获得所有的Contact

--- src/main/java/com/baldurtech/ContactServlet.java
+++ src/main/java/com/baldurtech/ContactServlet.java
@@ -62,9 +62,12 @@ public class ContactServlet extends HttpServlet
 		}
 		private List<Contact> getAllContacts()
 		{
-			List contacts = new ArrayList();
 			String sql = "select * from contact";
-			
+			return findAllContactsBySql(sql);
+		}
+		public List<Contact> findAllContactsBySql(String sql)
+		{
+			List<Contact> contacts = new ArrayList<Contact>();
 			DatabaseManager db = createDatabaseConnectionAndExecute(sql);
 			try
 			{
